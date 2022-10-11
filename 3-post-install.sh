723b17a28f3daaecb72f228e74a143364905b07b
ayman3amer <ayman.3amer96@gmail.com>
2022-10-11 03:32:48 +0200

    install dwm

--- 3-post-install.sh
+++ 3-post-install.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+# install dwm
+mkdir -vP ~/resources/github/
+git clone "https://github.com/ayman3amer/dwm.git" ~/resources/github/dwm
+cd ~/resources/github/dwm
+sudo make clean install
